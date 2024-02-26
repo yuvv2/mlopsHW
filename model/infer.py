6e9752bc9778ec54d11bee70cd7e0206d8b0f2e0
Artyom <amordashov@gmail.com>
2024-02-26 23:17:59 +0300

    hydra built

--- model/infer.py
+++ model/infer.py
@@ -1,11 +1,14 @@
 import os
 
+import hydra
 import joblib
 import pandas as pd
+from dvc.api import DVCFileSystem
+from omegaconf import DictConfig
 from sklearn.metrics import accuracy_score
 
 
-def model_infer(model, test_df: pd.DataFrame) -> pd.DataFrame:
+def model_infer(model, test_df: pd.DataFrame, cfg: DictConfig) -> pd.DataFrame:
     cat_cols = ["Sex", "Embarked"]
     num_cols = ["Pclass", "Age", "SibSp", "Parch", "Fare"]
     target_col = "Survived"
@@ -20,14 +23,21 @@ def model_infer(model, test_df: pd.DataFrame) -> pd.DataFrame:
     return preds
 
 
-if __name__ == "__main__":
+@hydra.main(config_path="./config", config_name="config", version_base="1.3")
+def main(cfg: DictConfig):
     current_file_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    data_path = os.path.join(current_file_dir, "data", "test.csv")
+    data_path = os.path.join(current_file_dir, cfg["data"]["path"], "test.csv")
     model_path = os.path.join(current_file_dir, "model", "trained_model.sav")
-
     preds_path = os.path.join(current_file_dir, "data", "predictions.csv")
+
+    fs = DVCFileSystem(os.path.join(current_file_dir, "data"))
+    fs.get_file("/data/test.csv", data_path)
 
     test_df = pd.read_csv(data_path)
     model = joblib.load(model_path)
 
-    predictions = pd.DataFrame(model_infer(model, test_df)).to_csv(preds_path)
+    pd.DataFrame(model_infer(model, test_df, cfg)).to_csv(preds_path)
+
+
+if __name__ == "__main__":
+    main()
--- model/train.py
+++ model/train.py
@@ -1,11 +1,14 @@
 import os
 
+import hydra
 import joblib
 import pandas as pd
+from dvc.api import DVCFileSystem
+from omegaconf import DictConfig
 from sklearn.ensemble import RandomForestClassifier
 
 
-def model_train(train_df: pd.DataFrame) -> RandomForestClassifier:
+def model_train(train_df: pd.DataFrame, cfg: DictConfig) -> RandomForestClassifier:
     cat_cols = ["Sex", "Embarked"]
     num_cols = ["Pclass", "Age", "SibSp", "Parch", "Fare"]
     target_col = "Survived"
@@ -14,16 +17,37 @@ def model_train(train_df: pd.DataFrame) -> RandomForestClassifier:
     X_train_h = pd.get_dummies(X_train, columns=cat_cols)
     y_train = train_df[target_col]
 
-    model = RandomForestClassifier(max_depth=15, random_state=42)
+    model = RandomForestClassifier(
+        n_estimators=cfg["model"]["n_estimators"],
+        criterion=cfg["model"]["criterion"],
+        min_samples_split=cfg["model"]["min_samples_split"],
+        min_samples_leaf=cfg["model"]["min_samples_leaf"],
+        min_weight_fraction_leaf=cfg["model"]["min_weight_fraction_leaf"],
+        max_features=cfg["model"]["max_features"],
+        min_impurity_decrease=cfg["model"]["min_impurity_decrease"],
+        bootstrap=cfg["model"]["bootstrap"],
+        oob_score=cfg["model"]["oob_score"],
+        verbose=cfg["model"]["verbose"],
+        warm_start=cfg["model"]["warm_start"],
+        ccp_alpha=cfg["model"]["ccp_alpha"],
+    )
     model.fit(X_train_h, y_train)
     return model
 
 
-if __name__ == "__main__":
+@hydra.main(config_path="./config", config_name="config", version_base="1.3")
+def main(cfg: DictConfig):
     current_file_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    data_path = os.path.join(current_file_dir, "data", "train.csv")
+    data_path = os.path.join(current_file_dir, cfg["data"]["path"], "train.csv")
     model_path = os.path.join(current_file_dir, "model", "trained_model.sav")
 
+    fs = DVCFileSystem(os.path.join(current_file_dir, "data"))
+    fs.get_file("/data/train.csv", data_path)
+
     train_df = pd.read_csv(data_path)
-    model = model_train(train_df)
+    model = model_train(train_df, cfg)
     joblib.dump(model, model_path)
+
+
+if __name__ == "__main__":
+    main()
